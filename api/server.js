567ccef11f7b91ad206206e9881243de407480c2
Shreyas <shreyassingh621@gmail.com>
2024-02-19 01:56:46 +0530

    Fetch NFt api created

--- api/server.js
+++ api/server.js
@@ -5,8 +5,20 @@ const ABI = require("./ABI.json")
 const app = express ()
 const web3 = new Web3("https://crimson-dawn-patina.ethereum-sepolia.discover.quiknode.pro/47d071f4d9ceb04a91320d33bb735897a7691c20/")
 const contractAddress = "0x97E53339B93381B2289D26CF5950187C3BBA4683"
+
 const contract = new web3.eth.Contract(ABI, contractAddress);
-console.log(contract)
+
+const fetchNFTs = async () => {
+    try {
+        const nftBalance = await contract.methods.balanceOf("0xd2675e4f825114C1DDEfa745D823f6448A8B1f7d")
+        return{userNFTs:nftBalance}
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+
 
 
 
