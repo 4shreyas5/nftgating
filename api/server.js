653a9db5ee522fe261ad7b1aa8617d28cd167c62
Shreyas <shreyassingh621@gmail.com>
2024-02-19 01:14:56 +0530

    Contract deployed and Used

--- api/server.js
+++ api/server.js
@@ -4,7 +4,9 @@ const ABI = require("./ABI.json")
 
 const app = express ()
 const web3 = new Web3("https://crimson-dawn-patina.ethereum-sepolia.discover.quiknode.pro/47d071f4d9ceb04a91320d33bb735897a7691c20/")
-
+const contractAddress = "0x97E53339B93381B2289D26CF5950187C3BBA4683"
+const contract = new web3.eth.Contract(ABI, contractAddress);
+console.log(contract)
 
 
 
